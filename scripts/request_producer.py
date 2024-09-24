c410d8d1ce880ad8a58bc22acd13631d43af35f3
guitarhead429 <guitarhead429@gmail.com>
2024-09-24 14:32:14 -0400

    initial commit

---
+++ scripts/request_producer.py
@@ -0,0 +1,15 @@
+import requests as rq
+import time
+
+def send_test_requests():
+    req = rq.get('HTTP://127.0.0.1:7878/request')
+    req = rq.post('HTTP://127.0.0.1:7878/request')
+    # print(req.status_code)
+
+def main():
+    print("Sending as many requests as possible...")
+    while True:
+        # print('Sleeping')
+        # time.sleep(1)
+        send_test_requests()
+main()
