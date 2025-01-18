d5b8476450966f78fe64c5ec0d39a5ae18c355bf
mnowzari <guitarhead429@gmail.com>
2025-01-18 17:51:28 -0500

    Removing ES stuff for now

--- src/logging.rs
+++ src/logging.rs
@@ -6,23 +6,18 @@ use crate::env::EnvLogLevel;
 // 
 // We grab log level from our config YAML file's env level
 // In the YAML file, we can also configure the details of an Elasticsearch instance.
-// This way, we can make a best-effort to dump logs into ES
-// There are probably better ways to do this via established libs, but I want to try my hand at it.
 
 
 // LogHandler should establish several things:
 // a. The log level
-// b. Are we dumping to ES?
 // Both of these should be found in the config YAML
 pub struct LogHandler {
-    dump_to_elasticsearch: bool, // indicates if we should use write to an Elasticsearch instance
     log_level: EnvLogLevel,
 }
 
 impl LogHandler {
     pub fn new(env_level: EnvLogLevel) -> Result<LogHandler, Box<dyn Error>> {
         Ok(LogHandler {
-            dump_to_elasticsearch: false,
             log_level: env_level,
         })
     }
